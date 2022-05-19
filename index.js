d4720c5349f08cf70dc5cb524187475ebba8862a
Trey <treyfields351@gmail.com>
2022-05-19 14:38:16 -0400

    commit tree a9350b906546d1ad4162f2249cfb126e8fac2cfb

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = () => 
+{
+  return tutorials.map((line) => 
+  {
+    const tokens = line.split(" ");
+    const capitalizedTokens = tokens.map
+    ((token) => token.charAt(0).toUpperCase() + token.slice(1)
+    );
+    const response = capitalizedTokens.join(" ");
+    return response;
+  });
+};
